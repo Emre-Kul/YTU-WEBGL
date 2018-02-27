69cd1e887cee776721badb27178044c0cc64b953
Emre-Kul <emrekul35@gmail.com>
2018-02-27 20:59:47 +0300

    triangle drawed

--- odev1/odev1.js
+++ odev1/odev1.js
@@ -1,21 +1,40 @@
 let gl;
+let vertices;
 
-const init = function(){
+const init = function () {
     const canvas = document.getElementById('gl-canvas');
-    
+
     gl = WebGLUtils.setupWebGL(canvas);
-    if(!gl)
+    if (!gl)
         alert("Webgl isn't avaliable!");
 
-    gl.viewport(0,0,canvas.width,canvas.height);
-    gl.clearColor(1.0,1.0,1.0,1.0);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    const program = initShaders(gl,'vertex-shader','fragment-shader');
+    const program = initShaders(gl, 'vertex-shader', 'fragment-shader');
     gl.useProgram(program);
+    //vertices
     
+    vertices = [
+        vec2(-1.0, 0.50),
+        vec2(-0.75, 1.0),
+        vec2(-0.50, 0.50)
+    ];
+    
+    // Load the data into the GPU
+    const bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+    const vPos = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPos);
+
     render();
 }
-const render = function(){
+const render = function () {
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
 }
 window.onload = init;
