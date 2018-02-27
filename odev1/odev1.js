b1c6c98eb5182ae4cbd82bca50c2eb1257d54fcc
Emre-Kul <emrekul35@gmail.com>
2018-02-27 22:54:53 +0300

    rotate problem solved and some small changes

--- odev1/odev1.js
+++ odev1/odev1.js
@@ -1,6 +1,6 @@
 let gl;
 let vertices = [];
-let triangleCount;
+const TWIST_ANGLE = 30;
 const createTriangle = function (center, size) {
     let height = Math.sqrt(3) * size;
     return [
@@ -10,21 +10,14 @@ const createTriangle = function (center, size) {
     ];
 }
 
-const myRotate = function (vec, center, angle) {//rotate defined
-    angle = angle * Math.PI / 180.0;
-    let mS = Math.sin(angle);
-    let mC = Math.cos(angle);
-    //x'=x*cos(@)-y*sin(@)
-    //y'=x*sin(@)+y*cos(@)
+const twistWithoutTesselation = function (vec, center, twist) {//rotate defined
+    let mS = Math.sin(twist * Math.PI / 180.0);
+    let mC = Math.cos(twist * Math.PI / 180.0);
     for (let i = 0; i < vec.length; i++) {
-        vec[i][0] -= center[0];
-        vec[i][1] -= center[1];
-        
-        vec[i][0] = vec[i][0] * mC - vec[i][1] * mS;
-        vec[i][1] = vec[i][0] * mS + vec[i][1] * mC;
-        
-        vec[i][0] += center[0];
-        vec[i][1] += center[1];
+        let tmpVec = vec2(vec[i][0] - center[0], vec[i][1] - center[1]);
+        vec[i][0] = tmpVec[0] * mC - tmpVec[1] * mS;
+        vec[i][1] = tmpVec[0] * mS + tmpVec[1] * mC;
+        vec[i] = vec2(vec[i][0] + center[0], vec[i][1] + center[1]);
     }
     return vec;
 }
@@ -42,10 +35,11 @@ const init = function () {
     const program = initShaders(gl, 'vertex-shader', 'fragment-shader');
     gl.useProgram(program);
 
-    vertices = createTriangle(vec2(-0.5, 0.5), 0.35);
-    let vertices2 = myRotate(createTriangle(vec2(-0.5, -0.5), 0.35),vec2(-0.5,-0.5), 30);
-    vertices = vertices.concat(vertices2);
-    triangleCount = 2;
+
+    let tr1 = createTriangle(vec2(-0.5, 0.5), 0.35);
+    let tr2 = twistWithoutTesselation(createTriangle(vec2(-0.5, -0.5), 0.35), vec2(-0.5, -0.5), TWIST_ANGLE);
+    vertices = vertices.concat(tr1).concat(tr2);
+
     // Load the data into the GPU
     const bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
@@ -62,7 +56,9 @@ const init = function () {
 const render = function () {
     console.log(vertices);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    for (let i = 0; i < triangleCount; i++)
-        gl.drawArrays(gl.TRIANGLE_FAN, i * 3, 3);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, 3);
+    gl.drawArrays(gl.TRIANGLE_FAN, 3, 3);
+
 }
 window.onload = init;
