d626197be383335f1e22b2a3ba27cb6d861412d5
Emre-Kul <emrekul35@gmail.com>
2018-03-01 22:30:47 +0300

    little speed improvments

--- odev1/odev1.js
+++ odev1/odev1.js
@@ -1,9 +1,10 @@
 let gl;
 
 const TWIST_ANGLE = 100;
+const SQRT_3 = Math.sqrt(3);
 
 const createTriangle = function (center, size) {
-    let height = Math.sqrt(3) * size;
+    let height = SQRT_3 * size;
     return [
         vec2(center[0] - size, center[1] - (height / 3.0)),//left
         vec2(center[0], center[1] + (height * 2.0 / 3.0)),//top
@@ -11,7 +12,7 @@ const createTriangle = function (center, size) {
     ];
 }
 const createReverseTriangle = function (center, size) {
-    let height = Math.sqrt(3) * size;
+    let height = SQRT_3 * size;
     return [
         vec2(center[0] - size, center[1] + (height * 2.0 / 3.0)),//left
         vec2(center[0], center[1] - (height / 3.0)),//top
@@ -21,8 +22,9 @@ const createReverseTriangle = function (center, size) {
 const twistWithoutTesselation = function (vec, center, twist) {//rotate defined
     let mS = Math.sin(twist * Math.PI / 180.0);
     let mC = Math.cos(twist * Math.PI / 180.0);
-    for (let i = 0; i < vec.length; i++) {
-        let tmpVec = vec2(vec[i][0] - center[0], vec[i][1] - center[1]);
+    let tmpVec, i;
+    for (i = 0; i < vec.length; i++) {
+        tmpVec = vec2(vec[i][0] - center[0], vec[i][1] - center[1]);
         vec[i][0] = tmpVec[0] * mC - tmpVec[1] * mS;
         vec[i][1] = tmpVec[0] * mS + tmpVec[1] * mC;
         vec[i] = vec2(vec[i][0] + center[0], vec[i][1] + center[1]);
@@ -37,10 +39,10 @@ const twistWithTesselation = function (vec, center, twist) {
         distance = sqrt(x*x+y*y)
     */
     twist = twist * Math.PI / 180.0;
-    let mS, mC, distance;
+    let i, tmpVec, mS, mC, distance;
 
-    for (let i = 0; i < vec.length; i++) {
-        let tmpVec = vec2(vec[i][0] - center[0], vec[i][1] - center[1]);
+    for (i = 0; i < vec.length; i++) {
+        tmpVec = vec2(vec[i][0] - center[0], vec[i][1] - center[1]);
         distance = Math.sqrt(tmpVec[0] * tmpVec[0] + tmpVec[1] * tmpVec[1]);
         mS = Math.sin(twist * distance);
         mC = Math.cos(twist * distance);
@@ -72,19 +74,15 @@ const createScene = function () {
                 );
             }
             else {
-                console.log("OK");
                 vertices = vertices.concat(
                     createReverseTriangle(vec2(x + size * yI, y), size)
                 )
-
                 twistedVertices = twistedVertices.concat(
                     twistWithTesselation(createReverseTriangle(vec2(x + size * yI, y - diff), size), trRightBottomCenter, TWIST_ANGLE)
                 );
-
             }
-
         }
-        y -= size * Math.sqrt(3);
+        y -= size * SQRT_3;
         x -= size;
     }
     twistedVertices = twistedVertices.concat(trLeftBottom);
@@ -121,15 +119,16 @@ const init = function () {
 }
 
 const render = function (size) {
+    let i;
     gl.clear(gl.COLOR_BUFFER_BIT);
     /*
     gl.drawArrays(gl.LINE_LOOP, 0, 3);
     gl.drawArrays(gl.TRIANGLE_FAN, 3, 3);
     */
-    for (let i = 0; i < size / 2; i += 3)
+    for (i = 0; i < size / 2; i += 3)
         gl.drawArrays(gl.LINE_LOOP, i, 3);
 
-    for (let i = size / 2; i < size; i += 3)
+    for (i = size / 2; i < size; i += 3)
         gl.drawArrays(gl.TRIANGLE_FAN, i, 3);
 
 
