67f26b9fdd4e57af20903be8d9be6d1484990fc1
Emre-Kul <emrekul35@gmail.com>
2018-03-18 00:13:58 +0300

    assigment2 cube drawed

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,16 +1,41 @@
 "use strict"
 let gl;
 
-const createGasket = function(center, size){
+const createGasket = function (center, size) {
 
 }
 
-const createCube = function(center, size){
-
+const createCube = function (center, size) {
+    const cubeCords = [
+        vec4(center[0] - size, center[1] - size, center[2] + size, 1.0),
+        vec4(center[0] - size, center[1] + size, center[2] + size, 1.0),
+        vec4(center[0] + size, center[1] + size, center[2] + size, 1.0),
+        vec4(center[0] + size, center[1] - size, center[2] + size, 1.0),
+        vec4(center[0] - size, center[1] - size, center[2] - size, 1.0),
+        vec4(center[0] - size, center[1] + size, center[2] - size, 1.0),
+        vec4(center[0] + size, center[1] + size, center[2] - size, 1.0),
+        vec4(center[0] + size, center[1] - size, center[2] - size, 1.0)
+    ];
+    const cubeIndices = [
+        //a, b, c, a, c, d
+        1, 0, 3, 1, 3, 2,
+        2, 3, 7, 2, 7, 6,
+        3, 0, 4, 3, 4, 7,
+        6, 5, 1, 6, 1, 2,
+        4, 5, 6, 4, 6, 7,
+        5, 4, 0, 5, 0, 1
+    ];
+    let i,
+        cubeVertices = [];
+    for (i = 0; i < cubeIndices.length; ++i) {
+        cubeVertices.push(cubeCords[cubeIndices[i]]);
+    }
+    return cubeVertices;
 }
 
 const createScene = function () {
-
+    let cube = createCube(vec3(-0.5, 0.0, 0.0), 0.3);
+    return cube;
 }
 
 const init = function () {
@@ -22,11 +47,11 @@ const init = function () {
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
 
     const program = initShaders(gl, 'vertex-shader', 'fragment-shader');
     gl.useProgram(program);
-
-    /*
+    let vertices = createScene();
 
     // Load the data into the GPU
     const bufferId = gl.createBuffer();
@@ -35,13 +60,14 @@ const init = function () {
 
     // Associate out shader variables with our data buffer
     const vPos = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPos, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPos);
-    */
-    render();
+
+    render(vertices.length);
 }
 
-const render = function () {
-    gl.clear(gl.COLOR_BUFFER_BIT);
+const render = function (size) {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, size);
 }
 window.onload = init;
